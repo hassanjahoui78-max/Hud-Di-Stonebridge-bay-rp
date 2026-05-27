d46ef9bde5ff4ea89b10a9afcdff1964bf7afb17
hassanjahoui78-max <hassanjahoui78@gmail.com>
2026-05-27 16:41:31 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,42 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+hidehudcomponents(2) -- armi
+
+CreateThread(function()
+    while true do
+        Wait(200) -- Aggiorna 5 volte al secondo
+        local playerPed = PlayerPedId()
+        
+        -- Dati Giocatore (Lavoro, Gang)
+        local PlayerData = QBCore.Functions.GetPlayerData()
+        
+        -- Se sei in un veicolo
+        if IsPedInAnyVehicle(playerPed, false) then
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+            local speed = GetEntitySpeed(vehicle) * 3.6 -- Da ms a km/h
+            local fuel = exports['LegacyFuel']:GetFuel(vehicle) -- Dipende dal tuo script benzina
+            
+            SendNUIMessage({
+                action = "updateCar",
+                inCar = true,
+                speed = speed,
+                fuel = fuel
+                -- La cintura richiede uno script a parte che invii true/false
+            })
+        else
+            SendNUIMessage({
+                action = "updateCar",
+                inCar = false
+            })
+        end
+    end
+end)
+
+CreateThread(function()
+    -- Questa nativa cambia il colore del tracciato Waypoint
+    -- Il codice colore 47 è l'arancione in GTA V
+    SetBlipRouteColour(GetFirstBlipInfoId(8), 47) 
+    
+    -- In alternativa, per forzare i colori dell'HUD nativo (HUD_COLOUR_ORANGE)
+    ReplaceHudColourWithRgba(142, 255, 159, 67, 255) 
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CrazieFan'
+description 'Stonebridge Bay HUD'
+
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/it.lua', 
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
